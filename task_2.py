bf4f67e54de7704784da9c91082fd60558c2bc14
evelisko <37504765+evelisko@users.noreply.github.com>
2020-02-11 14:26:48 +0400

    all task completed (task_2 - file corrected)

--- task_2.py
+++ task_2.py
@@ -23,17 +23,3 @@ while i < items_count:
 
 print(f'Преобразованный: {items}')
 
-# time_in_seconds = '100360'
-# print('Введено число: ' + time_in_seconds)
-# while not time_in_seconds.isdigit():
-#     time_in_seconds = input('Введите время в секундах: ')
-#
-# time_in_seconds = int(time_in_seconds)
-#
-# time_in_seconds /= 3600
-# hours = int(time_in_seconds)
-# time_in_seconds = (time_in_seconds - hours) * 60
-# minutes = int(time_in_seconds)
-# seconds = int((time_in_seconds - minutes) * 60)
-#
-# print("%02d:%02d:%02d" % (hours, minutes, seconds))
